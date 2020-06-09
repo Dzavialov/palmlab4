bdb400b407d250e69ed66618f08b2fa22f274972
dimaz0014 <58293091+dimaz0014@users.noreply.github.com>
2020-06-09 08:36:14 +0300

    Add files via upload

---
+++ lab 4/6.1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Draw();
+        }
+
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics graph = Graphics.FromImage(bmp);
+            Pen pen = new Pen(Color.Blue);
+            SolidBrush sld = new SolidBrush(Color.Blue);
+
+            Point Trpoint1 = new Point(100, 100); // ТРИКУТНИК
+            Point Trpoint2 = new Point(10, 120);
+            Point Trpoint3 = new Point(150, 150);
+
+            Point[] triangle =
+            {
+                Trpoint1,
+                Trpoint2,
+                Trpoint3
+            };
+            graph.FillPolygon(sld, triangle);
+
+            graph.DrawLine(pen, 140, 280, 150, 300); // РОМБ
+            graph.DrawLine(pen, 150, 300, 140, 320);
+            graph.DrawLine(pen, 140, 320, 130, 300);
+            graph.DrawLine(pen, 130, 300, 140, 280);
+            
+            graph.DrawArc(pen, 300, 100, 90, 120, 60, 60); // СЕКТОР ЕЛІПСА
+            graph.DrawLine(pen, 317,207, 343, 150);
+            graph.DrawLine(pen, 343, 150, 372, 207);
+
+            graph.DrawLine(pen, 400, 40, 500, 40); // ПРЯМОКУТНИК
+            graph.DrawLine(pen, 400, 100, 500, 100);
+            graph.DrawLine(pen, 400, 40, 400, 100);
+            graph.DrawLine(pen, 500, 40, 500, 100);
+
+            pictureBox1.Image = bmp;
+        }
+    }
+}
---
+++ lab 4/6.2/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            Draw();
+        }
+
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics graph = Graphics.FromImage(bmp);
+
+            SolidBrush GreenBrush = new SolidBrush(Color.Green);
+            SolidBrush BrownBrush = new SolidBrush(Color.Brown);
+            SolidBrush BlueBrush = new SolidBrush(Color.Blue);
+            SolidBrush YellowBrush = new SolidBrush(Color.Yellow);
+
+            Point point1 = new Point(300, 30);//ПЕРШИЙ ТРИКУТНИК
+            Point point2 = new Point(260, 70);
+            Point point3 = new Point(340, 70);
+
+            Point[] pts = { 
+                point1,
+                point2,
+                point3
+            };
+
+            graph.FillPolygon(GreenBrush, pts);
+
+
+            Point point12 = new Point(300, 50);//ДРУГИЙ ТРИКУТНИК
+            Point point22 = new Point(240, 120);
+            Point point32 = new Point(360, 120);
+
+            Point[] pts2 = {
+                point12,
+                point22,
+                point32
+            };
+
+            graph.FillPolygon(GreenBrush, pts2);
+
+
+            Point point123 = new Point(300, 80);//ТРЕТІЙ ТРИКУТНИК
+            Point point223 = new Point(220, 170);
+            Point point323 = new Point(380, 170);
+
+            Point[] pts3 = {
+                point123,
+                point223,
+                point323
+            };
+
+            graph.FillPolygon(GreenBrush, pts3);
+
+            Point crn1 = new Point(280, 170);
+            Point crn3 = new Point(280, 200);
+            Point crn2 = new Point(310, 170);
+            Point crn4 = new Point(310, 200);
+
+
+
+            Point[] crn = { 
+                crn1,
+                crn2,
+                crn4,
+                crn3
+                
+            };
+
+            graph.FillPolygon(BrownBrush, crn);
+
+            graph.FillEllipse(YellowBrush, 300, 80, 10, 10);
+            graph.FillEllipse(BlueBrush, 260, 150, 10, 10);
+            graph.FillEllipse(YellowBrush, 310, 140, 10, 10);
+            graph.FillEllipse(BlueBrush, 300, 120, 10, 10);
+            graph.FillEllipse(YellowBrush, 270, 120, 10, 10);
+
+            pictureBox1.Image = bmp;
+        }
+    }
+}
